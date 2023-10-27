906a8830e857ac422b2872cd100ebf5067bc1575
mmoramova <moramova@gmail.com>
2023-10-27 19:34:10 +0200

    CPP01/ex04

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmoramov <mmoramov@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 21:19:08 by mmoramov          #+#    #+#             */
+/*   Updated: 2023/10/27 19:33:26 by mmoramov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << "Error. Please enter 3 arguments: filename, s1, s2" << std::endl;
+		return 1;
+	}
+
+	std::string infileText;
+	std::string infileLine;
+	std::string fileName = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	std::size_t pos = 0;
+
+	std::ifstream infile (fileName, std::ifstream::in);
+
+	if (infile.is_open())
+	{
+		do {
+			getline(infile, infileLine);
+			infileText += infileLine + '\n';
+		} while (!infileLine.empty());
+		infileText.pop_back();
+	}
+	else
+	{
+		std::cout << "Error opening file." << std::endl;
+		return 1;
+	}
+	infile.close();
+
+	while ( (pos = infileText.find(s1, pos)) != std::string::npos )
+	{
+		infileText.erase(pos,s1.length());
+		infileText.insert(pos,s2);
+		pos += s2.length();
+	}
+	std::ofstream outfile (fileName + ".replace");
+	outfile << infileText;
+	outfile.close();
+	return 0;
+}
