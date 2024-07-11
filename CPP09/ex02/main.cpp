3804ae32860941287f99f31bfb0afa4504b7989d
mmoramova <moramova@gmail.com>
2024-07-11 18:15:33 +0200

    i am back, from linux.

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mmoramov <mmoramov@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 15:12:45 by mmoramov          #+#    #+#             */
-/*   Updated: 2024/03/19 19:13:07 by mmoramov         ###   ########.fr       */
+/*   Updated: 2024/07/11 18:15:15 by mmoramov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main (int argc, char **argv)
 {
 	if (argc < 2)
 	{
-		std::cout << "Incorrect input" << std::endl;
+		std::cout << "Incorrect input " << std::endl;
 		return (1);
 	}
 
